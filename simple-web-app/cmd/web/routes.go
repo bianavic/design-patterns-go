bb2699a7c93ecf1dc29c61acc36259f7bce26ca2
Fabi Ferlin <fabiana.ferlin@mercadolivre.com>
2024-07-27 14:51:00 -0300

    feat: add WebP and jpeg images

--- simple-web-app/cmd/web/routes.go
+++ simple-web-app/cmd/web/routes.go
@@ -4,6 +4,8 @@ import (
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"net/http"
+	"path/filepath"
+	"strings"
 	"time"
 )
 
@@ -13,6 +15,33 @@ func (app *application) routes() http.Handler {
 
 	mux.Use(middleware.Recoverer)
 	mux.Use(middleware.Timeout(60 * time.Second))
+
+	//fileServer := http.FileServer(http.Dir("./static"))
+	//mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
+
+	// FileServer handler with MIME type detection
+	mux.Handle("/static/*", http.StripPrefix("/static", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		path := r.URL.Path
+		ext := strings.ToLower(filepath.Ext(path))
+
+		// Set the Content-Type based on the file extension
+		switch ext {
+		case ".webp":
+			w.Header().Set("Content-Type", "image/webp")
+		case ".jpeg", ".jpg":
+			w.Header().Set("Content-Type", "image/jpeg")
+		case ".png":
+			w.Header().Set("Content-Type", "image/png")
+		case ".gif":
+			w.Header().Set("Content-Type", "image/gif")
+		default:
+			// Optionally handle other MIME types or use a default type
+			w.Header().Set("Content-Type", "application/octet-stream")
+		}
+
+		// Serve the file
+		http.ServeFile(w, r, "./static"+path)
+	})))
 
 	mux.Get("/", app.ShowHome)
 	mux.Get("/{page}", app.ShowPage)
--- simple-web-app/go.mod
+++ simple-web-app/go.mod
@@ -2,4 +2,4 @@ module simple-web-app
 
 go 1.22.3
 
-require github.com/go-chi/chi/v5 v5.1.0 // indirect
+require github.com/go-chi/chi/v5 v5.1.0
