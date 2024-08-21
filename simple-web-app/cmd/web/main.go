8513aba2cc2fe7eb84e86255bf4d64fa8b801411
Fabi Ferlin <fabiana.ferlin@mercadolivre.com>
2024-08-21 09:38:41 -0300

    feat: implement  streamer and set number of workers to 4

--- simple-web-app/cmd/web/main.go
+++ simple-web-app/cmd/web/main.go
@@ -9,6 +9,7 @@ import (
 	"net/http"
 	"simple-web-app/cmd/web/adapters"
 	"simple-web-app/configuration"
+	"simple-web-app/streamer"
 	"time"
 )
 
@@ -19,6 +20,7 @@ type application struct {
 	templateMap map[string]*template.Template
 	config      appConfig
 	App         *configuration.Application
+	videoQueue  chan streamer.VideoProcessingJob
 }
 
 type appConfig struct {
@@ -28,9 +30,15 @@ type appConfig struct {
 
 func main() {
 
+	const numWorkers = 4
+	// create a videoQueue and add to app
+	videoQueue := make(chan streamer.VideoProcessingJob, numWorkers)
+	defer close(videoQueue)
+
 	app := &application{
 		// return and populates its fields
 		templateMap: make(map[string]*template.Template),
+		videoQueue:  videoQueue,
 	}
 
 	flag.BoolVar(&app.config.useCache, "cache", false, "Use template cache")
@@ -50,6 +58,9 @@ func main() {
 	//xmlAdapter := adapters.RemoteService{Remote: &xmlBackend}
 
 	app.App = configuration.New(db, &jsonAdapter)
+
+	wp := streamer.New(videoQueue, numWorkers)
+	wp.Run()
 
 	server := &http.Server{
 		Addr:              port,
